f2ff223fdcaccd3ad6ccebe65938098560378e91
Pascal Greilich <pascal.greilich@gmail.com>
2021-12-11 13:46:45 -0500

    2021-10

--- 2021/src/day10.test.ts
+++ 2021/src/day10.test.ts
@@ -3,6 +3,6 @@ import { Day10 } from './day10';
 test('Day 10 sample input', () => {
   const [step1, step2] = new Day10().solve('data/day10.sample');
 
-  expect(step1).toBe(123);
-  expect(step2).toBe(456);
+  expect(step1).toBe(26397);
+  expect(step2).toBe(288957);
 });
--- 2021/src/day10.ts
+++ 2021/src/day10.ts
@@ -2,13 +2,83 @@ import { readFileSync } from 'fs';
 import { IDay } from './helpers';
 
 export class Day10 implements IDay {
-  solve(fileName: string) {
-    const nums = readFileSync(fileName, 'utf-8')
-      .split('\n')
-      .map((num) => parseInt(num));
+  pairs: Map<string, string>;
+  openers: Array<string>;
+  pointsPart1: Map<string, number>;
+  pointsPart2: Map<string, number>;
 
-    let result1 = 123;
-    let result2 = 456;
+  constructor() {
+    this.pairs = new Map([
+      ['[', ']'],
+      ['{', '}'],
+      ['(', ')'],
+      ['<', '>'],
+    ]);
+    this.openers = Array.from(this.pairs.keys());
+    this.pointsPart1 = new Map([
+      [')', 3],
+      [']', 57],
+      ['}', 1197],
+      ['>', 25137],
+    ]);
+    this.pointsPart2 = new Map([
+      [')', 1],
+      [']', 2],
+      ['}', 3],
+      ['>', 4],
+    ]);
+  }
+
+  part1(input: Array<string>) {
+    return input
+      .map((line) => {
+        let expected: Array<string> = [];
+        for (let char of line.split('')) {
+          if (this.openers.indexOf(char) > -1) {
+            expected = [this.pairs.get(char) ?? '', ...expected];
+          } else {
+            const popped = expected.shift();
+            if (char !== popped) {
+              return char;
+            }
+          }
+        }
+        return '';
+      })
+      .filter((invalidChar) => invalidChar.length > 0)
+      .reduce((acc, v) => acc + (this.pointsPart1.get(v) ?? 0), 0);
+  }
+
+  part2(input: Array<string>) {
+    const tmp = input
+      .map((line) => {
+        let expected: Array<string> = [];
+        for (let char of line.split('')) {
+          if (this.openers.indexOf(char) > -1) {
+            expected = [this.pairs.get(char) ?? '', ...expected];
+          } else {
+            const popped = expected.shift();
+            if (char !== popped) {
+              return [];
+            }
+          }
+        }
+        return expected;
+      })
+      .filter((result) => result.length > 0)
+      .map((chars) =>
+        chars.reduce((acc, v) => acc * 5 + (this.pointsPart2.get(v) ?? 0), 0)
+      )
+      .sort((a, b) => a - b);
+
+    return tmp[(tmp.length - 1) / 2];
+  }
+
+  solve(fileName: string) {
+    const input = readFileSync(fileName, 'utf-8').split('\n');
+
+    let result1 = this.part1(input);
+    let result2 = this.part2(input);
 
     return [result1, result2];
   }
