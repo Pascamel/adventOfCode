cc4b92b19ac3827a8b1b30e0e64b9d39e01f6c9d
Pascal Greilich <pascal.greilich@gmail.com>
2024-12-07 10:52:17 +0100

    prettier

--- 2024/src/day7.ts
+++ 2024/src/day7.ts
@@ -1,57 +1,59 @@
 import { readFileSync } from 'fs';
-import { IDay, Sum } from './helpers';
+import { IDay } from './helpers';
 
 type Line = {
   expected: number;
   numbers: number[];
-}
+};
 export class Day7 implements IDay<number[]> {
   solve(fileName: string) {
-    const lines = readFileSync(fileName, 'utf-8').split('\n').map((line) => {
-      const parts = line.split(': ');
-      return {
-        expected: parseInt(parts[0]),
-        numbers: parts[1].split(' ').map((x) => parseInt(x)),
-      };
-    });
+    const lines = readFileSync(fileName, 'utf-8')
+      .split('\n')
+      .map((line) => {
+        const parts = line.split(': ');
+        return {
+          expected: parseInt(parts[0]),
+          numbers: parts[1].split(' ').map((x) => parseInt(x)),
+        };
+      });
 
-    const plus = (numbers: number[]) => [numbers[0] + numbers[1], ...numbers.slice(2)];
+    const plus = ([a, b, ...list]: number[]) => [a + b, ...list];
 
-    const times = (numbers: number[]) => [numbers[0] * numbers[1], ...numbers.slice(2)];
+    const times = ([a, b, ...list]: number[]) => [a * b, ...list];
 
-    const pipe = (numbers: number[]) => [parseInt(`${numbers[0]}${numbers[1]}`), ...numbers.slice(2)];
+    const pipe = ([a, b, ...list]: number[]) => [parseInt(`${a}${b}`), ...list];
 
-    const solve = (line: Line, concat: boolean) => {      
-      if(line.numbers.length<1) {
+    const solve = (line: Line, concat: boolean) => {
+      if (line.numbers.length < 1) {
         return false;
-      } 
+      }
       if (line.numbers.length === 1) {
         return line.expected === line.numbers[0];
       }
-      
-      if (solve({...line, numbers: plus(line.numbers)}, concat)) {
-        return true;
-      } 
 
-      if (solve({...line, numbers: times(line.numbers)}, concat)) {
+      if (solve({ ...line, numbers: plus(line.numbers) }, concat)) {
         return true;
-      } 
+      }
+
+      if (solve({ ...line, numbers: times(line.numbers) }, concat)) {
+        return true;
+      }
 
       if (concat) {
-        if (solve({...line, numbers: pipe(line.numbers)}, concat)) {
+        if (solve({ ...line, numbers: pipe(line.numbers) }, concat)) {
           return true;
-        } 
+        }
       }
-      
+
       return false;
-    }
+    };
     const step1 = lines.reduce((acc, line) => {
-      return acc+(solve(line, false) ? line.expected : 0)
-    }, 0); 
-    
+      return acc + (solve(line, false) ? line.expected : 0);
+    }, 0);
+
     const step2 = lines.reduce((acc, line) => {
-      return acc+(solve(line, true) ? line.expected : 0)
-    }, 0); 
+      return acc + (solve(line, true) ? line.expected : 0);
+    }, 0);
 
     return [step1, step2];
   }
